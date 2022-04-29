ba0c687432caebeee1de1ce5b8603c84d33f0fab
AndreaCaval <andrea.cavalluzzo2000@gmail.com>
2022-04-29 10:22:52 +0200

    redux-thunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { setCurrentUser } from './store/user/user.action.js';
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from './utils/firebase/firebase.utils.js';
+import { setCurrentUser } from './store/user/user.action';
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from './utils/firebase/firebase.utils';
 
-import Home from "./routes/home/home.components.jsx";
-import Navigation from './routes/navigation/navigation.component.jsx';
-import Authentication from './routes/authentication/authentication.component.jsx';
-import Shop from './routes/shop/shop.component.jsx';
-import Checkout from './routes/checkouts/checkout.component.jsx';
+import Home from "./routes/home/home.component";
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
 
 
 const App = () => {
@@ -27,7 +27,7 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<Navigation />} >
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop/*' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
